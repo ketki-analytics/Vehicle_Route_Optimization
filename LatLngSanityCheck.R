428364c13e10f3710c7d99a82c199e570273709d
somsubhra88 <somsubhra.g@flipkart.com>
2016-05-06 17:29:11 +0530

    Update LatLngSanityCheck.R

--- LatLngSanityCheck.R
+++ LatLngSanityCheck.R
@@ -3,7 +3,7 @@ LatLngSanityCheck = function(latLngData,data)
   for(i in 2:NROW(latLngData))
   {
     t_Hub_j = time(point1 = as.list(latLngData[1,-1]),point2 = as.list(latLngData[i,-1]))
-    if(t_Hub_j > 2)
+    if(t_Hub_j > 3)
     {
       pincode = data %>% filter(.,order_external_id %in% as.character(latLngData[i,1])) %>% select(.,Pincode)
       pincode = as.character(pincode[1,1])
