d3de7b5545d56e04a6cd594dc357ded7a1a2aa25
somsubhra88 <somsubhra.g@flipkart.com>
2016-05-11 17:01:05 +0530

    V_2.0

--- LatLngSanityCheck.R
+++ LatLngSanityCheck.R
@@ -3,18 +3,21 @@ LatLngSanityCheck = function(latLngData,data)
   for(i in 2:NROW(latLngData))
   {
     t_Hub_j = time(point1 = as.list(latLngData[1,-1]),point2 = as.list(latLngData[i,-1]),avgSpeed = 1000*avgSpeed)
-    if(t_Hub_j > 3)
+    if(sum(t_Hub_j > 3) > 0)
     {
-      pincode = data %>% filter(.,order_external_id %in% as.character(latLngData[i,1])) %>% select(.,Pincode)
-      pincode = as.character(pincode[1,1])
-      R = LatLngFromPincode(pincode)
-      R$Lng = as.character(R$Lng)
-      R$Lat = as.character(R$Lat)
-      t_Hub_j_new = time(point1 = as.list(latLngData[1,-1]),point2 = R,avgSpeed = 1000*avgSpeed)
-      if(t_Hub_j_new + 0.5 < t_Hub_j)
+      if(t_Hub_j > 3)
       {
-        latLngData[i,"Lng"] = R$Lng
-        latLngData[i,"Lat"] = R$Lat
+        pincode = data %>% filter(.,order_external_id %in% as.character(latLngData[i,1])) %>% select(.,Pincode)
+        pincode = as.character(pincode[1,1])
+        R = LatLngFromPincode(pincode)
+        R$Lng = as.character(R$Lng)
+        R$Lat = as.character(R$Lat)
+        t_Hub_j_new = time(point1 = as.list(latLngData[1,-1]),point2 = R,avgSpeed = 1000*avgSpeed)
+        if(t_Hub_j_new + 0.5 < t_Hub_j)
+        {
+          latLngData[i,"Lng"] = R$Lng
+          latLngData[i,"Lat"] = R$Lat
+        }
       }
     }
   }
