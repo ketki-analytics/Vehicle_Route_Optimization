f63c75af04ef0c4b4141614f6047682e902e5650
somsubhra88 <somsubhra.g@flipkart.com>
2016-05-16 15:12:01 +0530

    V_2.1

--- optimizeVanStartTime.R
+++ optimizeVanStartTime.R
@@ -1,22 +1,21 @@
+slotDiff = function(v)
+{
+  iterData_1 = 
+    v %>%
+    routeSearch(timeMatrix,loadData,slotData,., deliveryTime = 25,L = 7.5,Q = 130) %>%
+    detailPath(.,timeMatrix,latLngData,loadData,slotData,vanStartTime,deliveryTime = 25) %>%
+    filter(.,from == 'Hub') %>% select(.,Slot_Start_Time,t_ij)
+  
+  slotDiff = iterData_1$Slot_Start_Time -(100/60)*iterData_1$t_ij
+ 
+  return(sum(slotDiff^2))
+}
+
 optmizeVanStartTime = function(route,timeMatrix,latLngData,loadData,slotData,vanStartTime,deliveryTime = 25,L = 7.5,Q = 130)
 {
-  
-  slotDiff = function(v)
-  {
-    iterData_1 = 
-      v %>%
-      routeSearch(timeMatrix,loadData,slotData,., deliveryTime = 25,L = 7.5,Q = 130) %>%
-      detailPath(.,timeMatrix,latLngData,loadData,slotData,vanStartTime,deliveryTime = 25) %>%
-      filter(.,from == 'Hub') %>% select(.,Slot_Start_Time,t_ij)
-    
-    slotDiff = iterData_1$Slot_Start_Time -(100/60)*iterData_1$t_ij
-   
-    return(sum(slotDiff^2))
-  }
-  
   counter = 0
   continueIteration = TRUE
-  while(continueIteration = TRUE)
+  while(continueIteration)
   {
     counter = counter + 1
     newVanStartTime = vanStartTime
