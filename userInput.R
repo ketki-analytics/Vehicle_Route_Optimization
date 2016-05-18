9973339d5bfb97864aba2f59f737f7787eaa16e8
somsubhra88 <somsubhra.g@flipkart.com>
2016-05-18 14:48:11 +0530

    V_2.1
    introduced maxDistCutOff

--- userInput.R
+++ userInput.R
@@ -18,6 +18,9 @@ shiftStart = c(900,1300) # for Bhiwandi
 # Average Speed for different Location
 avgSpeed = 15 # For Kudlu, Bhiwandi, Mandoli
 # avgSpeed = 20 # For Kolkata Only
+# Cut Off Time
+maxDistCutOff = 3 #Bangalore and Kolkata
+# maxDistCutOff = 4 #Bhiwandi and Mandoli
 ####################################################################################################
 ## If you have proir knowledge that these Orders do not require OFD, then put the Tracking IDs here
 notDeliveredOID = NULL
